199f4fd23b79081955c19198f83180ef4ae23732
sojiwyd <157882993+sojiwyd@users.noreply.github.com>
2026-04-16 01:57:29 +0700

    Delete check_admin.php

--- check_admin.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (session_status() !== PHP_SESSION_ACTIVE) {
-    session_start();
-}
-
-if (!isset($_SESSION['user_id']) || $_SESSION['user_role'] !== 'admin') {
-    die("ДОСТУП ЗАПРЕЩЕН. У вас нет прав администратора. <a href='login.php'>Войти</a>");
-}
